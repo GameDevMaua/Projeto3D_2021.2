deaf963d96f67a64588fbba7ddea2e51879faa82
Comunalti <frps2003@gmail.com>
2021-09-01 16:29:03 -0300

    Algumas alterações para debug

--- Projeto_3D_2021.2/Assets/Scripts/Main_Camera_Control.cs
+++ Projeto_3D_2021.2/Assets/Scripts/Main_Camera_Control.cs
@@ -19,6 +19,12 @@ public class Main_Camera_Control : MonoBehaviour{
         // Class.Event -= StartCameraCoroutine(intensity, frequency, timeInSeconds);
     }
 
+    [ContextMenu("Test Shake")]
+    public void TestShake()
+    {
+        StartShakeCameraCoroutine(1f,3f,.5f);
+    }
+
     private void StartShakeCameraCoroutine(float intensity, float frequency, float timeInSeconds) {
         StartCoroutine(ShakeCamera(intensity, frequency, timeInSeconds));
     }
@@ -26,14 +32,19 @@ public class Main_Camera_Control : MonoBehaviour{
     IEnumerator ShakeCamera(float intensity, float frequency, float timeInSeconds) {
         CinemachineBasicMultiChannelPerlin shakeParemeters =
             _virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
-
+        print(shakeParemeters);
+        
         shakeParemeters.m_AmplitudeGain = intensity;
         shakeParemeters.m_FrequencyGain = frequency;
 
-        yield return new WaitForSeconds(timeInSeconds);
-        
-        shakeParemeters.m_AmplitudeGain = 0f;
-        shakeParemeters.m_FrequencyGain = 0f;       
+        float resolution = 10;
+        for (float i = 0; i < resolution; i++)
+        {
+            yield return new WaitForSeconds(timeInSeconds/resolution);
 
+            var value = 1f - i / (resolution - 1);
+            shakeParemeters.m_AmplitudeGain = value;
+            shakeParemeters.m_FrequencyGain = value;
+        }
     }
 }
