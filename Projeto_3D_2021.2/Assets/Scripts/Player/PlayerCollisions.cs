5e50c901f22053f5742b7d666cc9ac07c3a319e2
Lucas Duez <lucas.verzaro@gmail.com>
2021-10-10 18:43:47 -0300

    PlayerCollisions
    Eu basicamente fiz um dicionário cuja a chave é a tag do objeto com o qual o player colidiu e o valor é uma função.
    
    Esse jeito é legal porque não tem nenhum if e até que é fácil extender o código, mas sinceramente, não sei como vai ficar legibilidade do código.

---
+++ Projeto_3D_2021.2/Assets/Scripts/Player/PlayerCollisions.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player{
+    public class PlayerCollisions : MonoBehaviour{
+        private Dictionary<string, Action> _collisionDictionary;
+        
+        private void OnCollisionEnter(Collision other) {
+            string gameTag = other.gameObject.tag;
+            
+                if (_collisionDictionary.ContainsKey(gameTag)) _collisionDictionary[gameTag]?.Invoke();
+                else {
+                    print("Essa colisao nao esta no dicionario");
+                }
+
+        }
+
+
+        private void Awake() {
+            _collisionDictionary = new Dictionary<string, Action> {
+                {"Car", () => print("Colisao com um carro")},
+
+                {"Teste", () => print("Colisao Teste")},
+                
+                {"Predio", ()=> print("Colisao com um predio")}
+                
+                
+            };
+
+        }
+    }
+}
